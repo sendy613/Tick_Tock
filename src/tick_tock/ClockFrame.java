b972997ccc823a1a92abafd13b486e8630fec451
Esti Sendrovitz <sendy613@gmail.com>
2015-03-09 14:55:11 -0400

    hello

--- src/tick_tock/ClockFrame.java
+++ src/tick_tock/ClockFrame.java
@@ -51,13 +51,11 @@ public class ClockFrame extends JFrame {
 		this.button = new JButton("Add Alarm");
 		Color blue = new Color(0, 204, 204);
 		button.setBackground(blue);
-		Integer[] hours = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-				12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
-		Integer[] minutes = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-				11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-				27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-				43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
-				59 };
+		Integer[] hours = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+				22, 23 };
+		Integer[] minutes = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+				21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+				47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 };
 		hoursList = new JComboBox<Integer>(hours);
 		minList = new JComboBox<Integer>(minutes);
 		hoursList.setBackground(blue);
@@ -85,11 +83,7 @@ public class ClockFrame extends JFrame {
 		snooze.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-				comp.getAlarms().snooze(comp.getClock().getTime());
-=======
 				comp.getAlarms().snooze();
->>>>>>> origin/master
 
 			}
 		});
@@ -113,8 +107,7 @@ public class ClockFrame extends JFrame {
 		inputPanel.add(minList);
 		inputPanel.add(button);
 		picturePanel = new JPanel();
-		picturePanel
-				.setLayout(new BoxLayout(picturePanel, BoxLayout.PAGE_AXIS));
+		picturePanel.setLayout(new BoxLayout(picturePanel, BoxLayout.PAGE_AXIS));
 		picturePanel.add(comp2);
 		buttonPanel.add(snooze);
 		buttonPanel.add(dismiss);
@@ -140,24 +133,17 @@ public class ClockFrame extends JFrame {
 				} catch (IOException e) {
 				}
 				while (true) {
-					if (frame.hoursList.isEnabled()
-							&& frame.comp.getAlarms().getCounter() > 10) {
+					if (frame.hoursList.isEnabled() && frame.comp.getAlarms().getCounter() > 10) {
 						frame.hoursList.setEnabled(false);
 						frame.minList.setEnabled(false);
 						frame.button.setEnabled(false);
-<<<<<<< HEAD
 					} else if (!frame.hoursList.isEnabled() && frame.comp.getAlarms().getCounter() < 10) {
-=======
-					} else if (!frame.hoursList.isEnabled()
-							&& frame.comp.getAlarms().getCounter() < 10) {
->>>>>>> origin/master
 						frame.hoursList.setEnabled(true);
 						frame.minList.setEnabled(true);
 						frame.button.setEnabled(true);
 					}
 					frame.comp.getClock().getTime().tick();
-					frame.comp.getAlarms().checkIfCurrentAlarms(
-							frame.comp.getClock().getTime());
+					frame.comp.getAlarms().checkIfCurrentAlarms(frame.comp.getClock().getTime());
 					frame.repaint();
 					if (frame.comp.getAlarms().getRing() == true) {
 						AudioPlayer.player.start(as);
@@ -165,14 +151,9 @@ public class ClockFrame extends JFrame {
 						frame.dismiss.setEnabled(true);
 					} else {
 						AudioPlayer.player.stop(as);
-<<<<<<< HEAD
-					}
-
-=======
 						frame.snooze.setEnabled(false);
 						frame.dismiss.setEnabled(false);
 					}
->>>>>>> origin/master
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
